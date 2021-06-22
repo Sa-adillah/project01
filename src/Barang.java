06439a3b791eeee86dbdf4255a51ddd6b453805d
Sa-adillah <63109326+Sa-adillah@users.noreply.github.com>
2021-06-22 19:54:47 +0800

    Initial commit

---
+++ src/Barang.java
@@ -0,0 +1,11 @@
+public class Barang {
+    public String kodeBarang;
+    public String namaBarang;
+    public String jenisBarang;
+    public String merek;
+    public int stok;
+    public int hargaBeli;
+    public int hargaJual;
+    public int diskon;
+
+}
